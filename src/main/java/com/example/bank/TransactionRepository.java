d44b39f99631625df66556aec5cad0a3707c5197
spikepiece <oguzerenacar@gmail.com>
2025-11-28 03:42:46 +0100

    aaa

--- src/main/java/com/example/bank/TransactionRepository.java
+++ src/main/java/com/example/bank/TransactionRepository.java
@@ -1,5 +1,6 @@
 package com.example.bank;
 
+import com.example.bank.TransactionService;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TransactionRepository extends JpaRepository<Transactions, Integer> {
