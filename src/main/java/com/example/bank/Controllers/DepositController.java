3594d48b2d80cdb51e8f8a4fe28e6210ad29a593
spikepiece <oguzerenacar@gmail.com>
2025-12-16 01:00:10 +0100

    deposit

--- src/main/java/com/example/bank/Controllers/DepositController.java
+++ src/main/java/com/example/bank/Controllers/DepositController.java
@@ -1,4 +1,21 @@
 package com.example.bank.Controllers;
 
+import com.example.bank.Entities.Accounts;
+import com.example.bank.Services.DepositService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/deposit")
 public class DepositController {
+    private final DepositService depositService;
+
+    public DepositController(DepositService depositService) {
+        this.depositService = depositService;
+    }
+
+    @PostMapping("/{id}/{amount}")
+    public Accounts deposit(@PathVariable Integer id, @PathVariable Double amount){
+        return depositService.deposit(id, amount);
+    }
+
 }
--- src/main/java/com/example/bank/Services/DepositService.java
+++ src/main/java/com/example/bank/Services/DepositService.java
@@ -1,4 +1,27 @@
 package com.example.bank.Services;
 
+import com.example.bank.Entities.Accounts;
+import com.example.bank.Entities.Transactions;
+import com.example.bank.Repositories.AccountsRepository;
+import com.example.bank.Repositories.TransactionRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class DepositService {
+
+    private final AccountsRepository accountsRepository;
+    private TransactionRepository transactionRepository;
+
+    public DepositService(AccountsRepository accountsRepository, TransactionRepository transactionRepository) {
+        this.accountsRepository = accountsRepository;
+        this.transactionRepository = transactionRepository;
+    }
+
+    public Accounts deposit(Integer id, Double amount) {
+        Accounts account = accountsRepository.findById(id).
+                orElseThrow(() -> new IllegalStateException("Not Found"));
+        transactionRepository.save(new Transactions(null, id, "DEPOSIT", amount, "test"));
+        account.setBalance(account.getBalance() + amount);
+        return accountsRepository.save(account);
+    }
 }
